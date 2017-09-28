5243c2697d1e94d8984d40b00a4b154415e57e20
amlyje <31208046+amlyje@users.noreply.github.com>
2017-09-28 14:16:01 +0200

    Udvidet søgning - boks (med funktioner)
    Fælles - Amalie kodede

--- js/script.js
+++ js/script.js
@@ -16,3 +16,36 @@ function openTab(evt, tab) {
 }
 
 document.getElementById("defaultOpen").click();
+
+
+new Vue({
+	el: '#productResult',
+	
+	data: {
+	productItems:[
+		],
+	newProduct:'',	
+	},
+	
+	
+	methods: {
+
+//Methods for Preperation		
+	addProduct: function () {
+			var productItem = this.newProduct.trim();
+			if (productItem) {
+				this.productItems.push({
+					text: productItem,
+					checked: false
+				});
+				this.newProduct = "";
+			}
+		},
+
+		removeProductItems: function (productItem) {
+			var index = this.productItems.indexOf(productItem);
+			this.productItems.splice(index, 1);
+		},
+	
+}
+});
