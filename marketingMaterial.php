299d3f9093a9140cd7b7ef754603c124212c1dc7
Carsten <carstena1994@gmail.com>
2017-10-02 12:17:06 +0200

    bug fix

--- marketingMaterial.php
+++ marketingMaterial.php
@@ -2,7 +2,7 @@
   <p>Search by product number or name</p>
   <section class="prefetch">
     <input class="typeahead inputPump inputLarge">
-    <button type="button" id="submit" class="iconButton" name="button"><i class="fa fa-search" aria-hidden="true"></i></button>
+    <button type="button" id="submit" onclick="submitPump()" class="iconButton" name="button"><i class="fa fa-search" aria-hidden="true"></i></button>
     <button type="button" id="advancedSearch" name="button">Advanced Search</button>
   </section>
   <section class="show displayNone">
@@ -16,7 +16,7 @@
         <p class="padding-bottom">Search ‘All’ or add product(s) to search list</p>
 
         <div id="searchBar">
-          <input id="addProduct" onclick="submitPump()" class="awesomplete inputPump inputSmall" data-list="Alpha 1, Alpha 2, Alpha 3" v-model="newProduct" @keyup.enter="addProduct" placeholder="Add product(s)">
+          <input id="addProduct" class="awesomplete inputPump inputSmall" data-list="Alpha 1, Alpha 2, Alpha 3" v-model="newProduct" @keyup.enter="addProduct" placeholder="Add product(s)">
           <button @click="addProduct" class="iconButton"><i class="fa fa-plus" aria-hidden="true"></i></button>
         </div>
 
