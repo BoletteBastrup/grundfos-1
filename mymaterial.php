dc60d07f1a936efb73f8ddf7d97629af45e10896
soerenheltborg <sheltborg@icloud.com>
2017-10-02 09:34:38 +0200

    html til my material
    Søren

--- mymaterial.php
+++ mymaterial.php
@@ -1,3 +1,78 @@
 <article id="My material" class="tabcontent">
-  <h3>My Material</h3>
+  <form>
+    <h3>My pump models</h3>
+    <ul>
+      <li><input type="checkbox">ALPHA 2</li>
+      <li><input type="checkbox">ALPHA 3</li>
+      <li><input type="checkbox">CONLIFT</li>
+      <li><input type="checkbox">CRN</li>
+      <li><input type="checkbox">Magna</li>
+      <li><input type="checkbox">SEG</li>
+      <li><input type="checkbox">SPK</li>
+      <li><input type="checkbox">SQU</li>
+      <li><input type="checkbox">TP</li>
+      <li><input type="checkbox">UNILIFT AP</li>
+      <li><input type="checkbox">UNILIFT CC</li>
+      <li><input type="checkbox">UNILIFT KP</li>
+      <li><input type="checkbox">UP</li>
+    </ul>
+  </form>
+  <section class="my-material">
+    <article class="product-thumb">
+      <img src="img/products/alpha_2.png" alt="Alpha 2">
+      <h3>ALPHA 2</h3>
+    </article>
+    <article class="product-thumb">
+      <img src="img/products/alpha_3.jpg" alt="Alpha 3">
+      <h3>ALPHA 3</h3>
+    </article>
+    <article class="product-thumb">
+      <img src="img/products/cmbe.png" alt="cmbe">
+      <h3>CMBE</h3>
+    </article>
+    <article class="product-thumb">
+      <img src="img/products/alpha_3.jpg" alt="Alpha 3">
+      <h3>CONLIFT</h3>
+    </article>
+    <article class="product-thumb">
+      <img src="img/products/alpha_2.png" alt="Alpha 2">
+      <h3>CRN</h3>
+    </article>
+    <article class="product-thumb">
+      <img src="img/products/alpha_3.jpg" alt="Alpha 3">
+      <h3>MAGNA</h3>
+    </article>
+    <article class="product-thumb">
+      <img src="img/products/cmbe.png" alt="cmbe">
+      <h3>SEG</h3>
+    </article>
+    <article class="product-thumb">
+      <img src="img/products/alpha_3.jpg" alt="Alpha 3">
+      <h3>SPK</h3>
+    </article>
+    <article class="product-thumb">
+      <img src="img/products/alpha_2.png" alt="Alpha 2">
+      <h3>SQE</h3>
+    </article>
+    <article class="product-thumb">
+      <img src="img/products/alpha_3.jpg" alt="Alpha 3">
+      <h3>TP</h3>
+    </article>
+    <article class="product-thumb">
+      <img src="img/products/cmbe.png" alt="cmbe">
+      <h3>UNILIFT AP</h3>
+    </article>
+    <article class="product-thumb">
+      <img src="img/products/alpha_3.jpg" alt="Alpha 3">
+      <h3>UNILIFT CC</h3>
+    </article>
+    <article class="product-thumb">
+      <img src="img/products/alpha_2.png" alt="Alpha 2">
+      <h3>UNILIFT KP</h3>
+    </article>
+    <article class="product-thumb">
+      <img src="img/products/alpha_3.jpg" alt="Alpha 3">
+      <h3>UP</h3>
+    </article>
+  </section>
 </article>
